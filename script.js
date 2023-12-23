937e295fc9281c70c4920257746c5acd5e70fef9
davisaugust <davisaugust004@gmail.com>
2023-12-23 20:43:38 -0300

    Making the function 'on click' with js.

--- script.js
+++ script.js
@@ -1,41 +1,68 @@
 const grid = document.querySelector('.game');
 
 const characters = [
-    'charizard' ,
+    'Charizard' ,
     'umbreon' ,
     'snorlax1' ,
-    'blastoise',
+    'Blastoise',
     'lucario',
     'mewtwo',
-    'Venusaur2',
+    'Venusaur',
     'zapdos',
 ];
 
 const createElement = (tag, className) => {
-    const element = document.creteElement(tag);
+    const element = document.createElement(tag);
     element.className = className;
     return element;
 }
 
+let firstCard = '';
+let secondCard = '';
 
-const createCard = () => {
+const revealCard = ({ target }) => {
+
+    if (target.parentNode.className.includes('reveal_card')){
+        return;
+    }
+
+    if (firstCard === ''){
+
+        target.parentNode.classList.add('reveal_card');
+
+    } else if (secondCard === ''){
+
+        target.parentNode.classList('reveal_card')
+        secondCard = target.parentNode;
+    }
+
+    target.parentNode.classList.add('reveal_card');
+}
+const createCard = (character) => {
 
     const card = createElement('div', 'card')
     const front = createElement('div', 'face card_front')
     const back = createElement('div', 'face card_back')
 
+    front.style.backgroundImage = `url('/images/${character}.jpeg')`;
 
     card.appendChild(front);
-    card.appendChild(back);
+    card.appendChild(back); 
+
+    card.addEventListener('click', revealCard);
 
     return card;
 }
 
 const loadGame = () => {
-    
-    characters.forEach((character) => {
 
-        const card = createCard();
+    const duplicateCharacters = [ ...characters, ...characters ]
+
+    const shuffledArray = duplicateCharacters.sort(() => Math.random() - 0.5);
+    
+    shuffledArray.forEach((character) => {
+
+        const card = createCard(character);
         grid.appendChild(card);
 
     });
